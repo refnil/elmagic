cad888603db0a12787ceaad9930c0f9326fff57d
Martin Lavoie <broemartino@gmail.com>
2015-06-24 12:03:37 -0400

    Added test macro

--- apps/mtg_parser/lib/mtg_parser.ex
+++ apps/mtg_parser/lib/mtg_parser.ex
@@ -1,2 +1,18 @@
 defmodule MtgParser do
+
+  def parse!(text) do
+    {:ok,result} = parse(text)
+    result
+  end
+
+  def parse(text) when is_list text do
+    {:ok, tokens, _} = text |> :mtg_lexer.string
+    :mtg_parser.parse(tokens)
+  end
+
+  def parse(text) when is_binary text do
+    text |> to_char_list |> parse
+  end
+
+  defmacro test, do: 2
 end
