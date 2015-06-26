7fffb032c40f9ba0ebb386609ea0c38eba9a3ce2
Martin Lavoie <broemartino@gmail.com>
2015-06-25 23:07:52 -0400

    Keyword list

--- apps/mtg_parser/lib/mtg_parser.ex
+++ apps/mtg_parser/lib/mtg_parser.ex
@@ -7,4 +7,23 @@ defmodule MtgParser do
     between(char("{"), one_of("WURBG"), char("}"))
   end
 
+  defmparser target do
+    zero
+  end
+
+  defmparser caracteristic do
+    zero
+  end
+
+  defmparser object do
+    zero
+  end
+
+  defmparser int do
+    zero
+  end
+
+  defmparser mana_cost do
+    zero
+  end
 end
