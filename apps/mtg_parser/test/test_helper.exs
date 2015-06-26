dbdd1db3d3b753e5e1774992c3b88ff92fded92c
Martin Lavoie <broemartino@gmail.com>
2015-06-25 20:55:46 -0400

    Generate many test from a list

--- apps/mtg_parser/test/test_helper.exs
+++ apps/mtg_parser/test/test_helper.exs
@@ -3,28 +3,30 @@ ExUnit.start
 defmodule ParserTestMacro do
   import ExUnit.Assertions
 
-  def parse_test_fun(parser, list) do
-    for element <- list do
-      {value, result} = case element do
-        {c,r}   -> {c,r}
-        e       -> flunk("Wrong format in element list. " <> inspect(e))
-      end
-        
-      received = ExParsec.parse_value(value,parser)
-      assert(received == {:ok,nil,result}, 
-        value <> " could not be parsed.\n" <> 
-        "Received: " <> inspect(received) <> "\n" <>
-        "Instead of: " <> inspect(result)
-      )
+  def parse_test_fun(parser, element) do
+    {value, result} = case element do
+      {c,r}   -> {c,r}
+      e       -> flunk("Wrong format in element list. " <> inspect(e))
     end
+      
+    received = ExParsec.parse_value(value,parser)
+    assert(received == {:ok,nil,result}, 
+      value <> " could not be parsed.\n" <> 
+      "Received: " <> inspect(received) <> "\n" <>
+      "Instead of: " <> inspect(result)
+    )
   end
 
   defmacro parse_test(testName, parser, clause) do
-    list_value = Keyword.get(clause, :do, clause)
+      list_quoted = Keyword.get(clause, :do, clause)
+      {list_value,[]} = Code.eval_quoted(list_quoted)
 
-    quote do
-      test unquote(testName) do
-        parse_test_fun(unquote(parser), unquote(list_value))
+      for element <- list_value do
+        {value,_result} = element
+        quote do
+         test unquote(testName <> " " <> value) do
+            parse_test_fun(unquote(parser), unquote(element))
+         end
       end
     end
   end
