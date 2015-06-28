092ded59022f7274c3d2730aa3e7e60a3c1a9183
Mélodie Lapointe <mlapointe923@hotmail.com>
2015-06-28 14:20:50 -0400

    Add Triggered habilities

--- apps/mtg_parser/lib/mtg_parser/triggered.ex
+++ apps/mtg_parser/lib/mtg_parser/triggered.ex
@@ -2,5 +2,15 @@ defmodule MtgParser.Triggered do
   import ExParsec.Base
   import ExParsec.Helpers
   import ExParsec.Text
+  
+  import MtgParser.Event
+  import MtgParser.Effect
 
+  defmparser triggered do
+    ev <-event
+    char(",")
+    space
+    ef <- effect
+    return {ev,ef}
+  end
 end
--- apps/mtg_parser/test/mtg_parser/tiggered_test.exs
+++ apps/mtg_parser/test/mtg_parser/tiggered_test.exs
@@ -3,6 +3,12 @@ defmodule MtgParser.Triggered.Test do
 
   import ParserTestMacro
   import MtgParser.Triggered
+  
+  parse_test "Triggered abilities", triggered do
+    [
+      "When Mardu Blazebringer attacks or blocks, sacrifice it at end of combat."
+    ]
+  end
 
 end
 
